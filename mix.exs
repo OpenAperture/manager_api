32986942e702dc4b32ab9118362cda992949fa6c
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-04-17 15:38:26 -0500

    add exdoc deps

--- mix.exs
+++ mix.exs
@@ -29,6 +29,8 @@ defmodule ManagerApi.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
+      {:ex_doc, github: "elixir-lang/ex_doc", only: [:test]},
+      {:markdown, github: "devinus/markdown", only: [:test]},      
       {:json, "~>0.3.0"},
       {:uuid, "~> 0.1.5" },
       {:openaperture_auth, git: "https://#{System.get_env("GITHUB_OAUTH_TOKEN")}:x-oauth-basic@github.com/OpenAperture/auth.git",
